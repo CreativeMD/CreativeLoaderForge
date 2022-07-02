1e3e37ab604b7acf7c45387f420ad05c7f9fdcb3
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-07-02 09:45:42 +0200

    Added more functionality to CreativeBlockModel

--- src/main/java/team/creative/creativecore/client/render/model/CreativeBakedBoxModel.java
+++ src/main/java/team/creative/creativecore/client/render/model/CreativeBakedBoxModel.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.client.Minecraft;
@@ -13,13 +16,17 @@ import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.client.model.data.EmptyModelData;
+import net.minecraftforge.client.model.data.IModelData;
 import team.creative.creativecore.client.render.box.RenderBox;
 import team.creative.creativecore.common.util.math.base.Facing;
 import team.creative.creativecore.common.util.mc.ColorUtils;
@@ -29,6 +36,9 @@ public class CreativeBakedBoxModel extends CreativeBakedModel {
     public static Minecraft mc = Minecraft.getInstance();
     
     public static List<BakedQuad> compileBoxes(List<? extends RenderBox> boxes, Facing side, RenderType layer, RandomSource rand, boolean item) {
+        if (side == null)
+            return Collections.EMPTY_LIST;
+        
         List<BakedQuad> baked = new ArrayList<>();
         for (int i = 0; i < boxes.size(); i++) {
             RenderBox box = boxes.get(i);
@@ -82,12 +92,18 @@ public class CreativeBakedBoxModel extends CreativeBakedModel {
     }
     
     @Override
-    @Deprecated
-    public List<BakedQuad> getQuads(BlockState state, Direction direction, RandomSource rand) {
+    public @NotNull IModelData getModelData(@NotNull BlockAndTintGetter level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull IModelData modelData) {
+        if (block != null)
+            return block.getModelData(level, pos, state, modelData);
+        return modelData;
+    }
+    
+    @Override
+    public @NotNull List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction direction, @NotNull RandomSource rand, @NotNull IModelData extraData) {
         Facing facing = Facing.get(direction);
         if (state != null) {
             if (block != null)
-                return compileBoxes(block.getBoxes(state), facing, MinecraftForgeClient.getRenderType(), rand, false);
+                return compileBoxes(block.getBoxes(state, extraData, rand), facing, MinecraftForgeClient.getRenderType(), rand, false);
             return Collections.EMPTY_LIST;
         }
         
@@ -106,6 +122,12 @@ public class CreativeBakedBoxModel extends CreativeBakedModel {
         }
         
         return Collections.EMPTY_LIST;
+    }
+    
+    @Override
+    @Deprecated
+    public List<BakedQuad> getQuads(BlockState state, Direction direction, RandomSource rand) {
+        return getQuads(state, direction, rand, EmptyModelData.INSTANCE);
     }
     
 }
